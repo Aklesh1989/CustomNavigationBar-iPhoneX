7a90ec38f074bf462aef373e637b534fded22ffd
aklesh1989 <akleshrathaur23@github.com>
2018-01-22 18:13:23 +0530

    .

--- iPhone-X-Custom-bar/AppDelegate.swift
+++ iPhone-X-Custom-bar/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        print("Application did finish launching")
+        print("Application did finish launching Master.")
 
         // Override point for customization after application launch.
         return true
